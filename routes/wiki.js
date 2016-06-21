5af6998011ca0df70b888dc1e9b2cbd987874e86
Eli Goldberg <eli.b.goldberg@gmail.com>
2016-06-22 02:23:30 +0300

    code foramtting - generators

--- routes/wiki.js
+++ routes/wiki.js
@@ -13,6 +13,40 @@ function validateParams(params) {
   return err;
 }
 
+function formatData(response) {
+  return function* () {
+    var body = [];
+    if (response.body.query) {
+      const {query: {pages: data}} = JSON.parse(response.body);
+
+      let results = Object.keys(data)
+        .map((val) => {
+
+          const item = data[val];
+
+          // nested-destructuring default value bug forces to check this 
+          if (!item.thumbnail) {
+            item.thumbnail = { source: '' };
+          }
+
+          try {
+            var { title, thumbnail: {source: thumbnailUri}, coordinates: [{lat, lon}]} = item;
+            return {
+              title,
+              thumbnailUri,
+              coordinates: { lat, lon }
+            };
+          } catch (err) {
+            console.log(err);
+          }
+        });
+
+      body = results;
+    }
+    yield body;
+  }
+}
+
 router.get('/wikiNearby', function* (next) {
   const params = this.query;
   const err = validateParams(params);
@@ -25,36 +59,7 @@ router.get('/wikiNearby', function* (next) {
   const ggscoord = `${params.lat}|${params.lon}`;
   wikiMedia.qs['ggscoord'] = ggscoord;
   const response = yield request(wikiMedia);
-  if (!response.body.query) {
-    this.body = [];
-  } else {
-
-    const {query: {pages: data}} = JSON.parse(response.body);
-
-    let results = Object.keys(data)
-      .map((val) => {
-
-        const item = data[val];
-
-        // nested-destructuring default value bug forces to check this 
-        if (!item.thumbnail) {
-          item.thumbnail = { source: '' };
-        }
-
-        try {
-          var { title, thumbnail: {source: thumbnailUri}, coordinates: [{lat, lon}]} = item;
-          return {
-            title,
-            thumbnailUri,
-            coordinates: { lat, lon }
-          };
-        } catch (err) {
-          console.log(err);
-        }
-      });
-
-    this.body = results;
-  }
+  this.body = yield formatData(response);
   this.status = 200;
 });
 
